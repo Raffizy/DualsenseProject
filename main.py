3bcb37294b853c618675349bbcfaa74802f3e40e
Raffizy <rafizkhan164@gmail.com>
2026-04-18 14:47:13 +0100

    Created a basic trigger systems that responds to controller input

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+from pydualsense import *
+import time
+# get dualsense instance
+ds = pydualsense()
+# initialize controller and connect
+ds.init()
 
-from pydualsense import pydualsense, TriggerModes
+print('Drive Simulation prototype')
 
-def cross_pressed(state):
-    print(state)
+last_square= False
+last_cross= False
 
-ds = pydualsense() # open controller
-ds.init() # initialize controller
-state = "active"
-ds.cross_pressed += cross_pressed
-ds.light.setColorI(255,0,0) # set touchpad color to red
-ds.triggerL.setMode(TriggerModes.Rigid)
-ds.triggerL.setForce(1, 255)
+#switch Force
+Force=1
 
-if bool(state) == False:
-    ds.close()
-    # closing the controller   
+ds.triggerR.setMode(TriggerModes.Pulse)
+#debug command+ main loop
+# loop until r1 is pressed to feel effect
+while not ds.state.R1:
+    ##gear shift simulation
+    if ds.state.square and not last_square:
+        Force += 1
+        
+        Force = min(Force, 6)
+        print("Force set to ", Force)
 
- 
+    if ds.state.cross and not last_cross:
+        Force -= 1
+        Force = max(Force, 1)
+        print("Force set to ", Force)
+
+    last_square = ds.state.square
+    last_cross = ds.state.cross
+    #Set L trigger as to simulate break
+    #ds.triggerL.setMode(TriggerModes.Rigid)
+    #ds.triggerL.setForce(1, 255)
+
+    # set R trigger to simulate road feel, clutch
+    ds.triggerR.setForce(Force, 255)
+    time.sleep(0.01)
+
+
+# terminate the thread for message and close the device
+
+ds.close()
+
+print("Device Closed")
